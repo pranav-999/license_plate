2e59b87107467039cb359e4dee4029110a715d92
Pranav <90954091+pranav-999@users.noreply.github.com>
2025-04-02 14:52:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ from PIL import Image
 from ultralytics import YOLO
 from paddleocr import PaddleOCR
 
+import asyncio
+
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    
 # --------------------- Image Enhancement Functions ---------------------
 def apply_clahe(image):
     """Apply CLAHE to enhance contrast"""
